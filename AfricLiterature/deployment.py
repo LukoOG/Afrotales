742ae9f9d3aac437414c5c273fd7616c7181918f
LukoOG <emmanueladesipe01@gmail.com>
2024-05-08 11:00:28 +0100

    edit deployment

--- AfricLiterature/deployment.py
+++ AfricLiterature/deployment.py
@@ -11,7 +11,7 @@ DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
